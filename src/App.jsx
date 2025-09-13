991e5f25d8ec9bd543ead1d9d16ec8f0a403c3e6
Mariano Juarez <cm.juarez.95@gmail.com>
2025-09-13 15:34:18 -0300

    se creo la tabla, los botones y algunas filas de ejemplo

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { Container } from "react-bootstrap";
 import Menu from "./components/shared/Menu";
 import Footer from "./components/shared/Footer";
+import Administrador from "./components/pages/Administrador";
 
 export default function AppLayout() {
   return (
     <>
       <Menu />
-      <main style={{ padding: "2rem 0", flex: "1" }}>
+      {/* <main style={{ padding: "2rem 0", flex: "1" }}>
         <Container>
           <p className="text-center fs-5">Hola Aqui va el MAIN</p>
         </Container>
-      </main>
+      </main> */}
+      <Administrador></Administrador>
       <Footer />
     </>
   );
--- src/components/pages/Administrador.jsx
+++ src/components/pages/Administrador.jsx
@@ -1,10 +1,75 @@
 import React from 'react';
+import { Button, Table } from "react-bootstrap";
 
 const Administrador = () => {
     return (
-        <div>
-            
-        </div>
+        <section className='container'>
+            <div className='d-flex justify-content-between mt-4'>
+                <h1>Administración</h1>
+                <Button variant='success' className='py-0'>Agregar Canción</Button>
+            </div>
+
+            <Table responsive striped bordered hover variant="dark" className='mt-4'>
+                <thead>
+                    <tr className="text-center">
+                        <th>Código</th>
+                        <th>Título</th>
+                        <th>Artista/Grupo</th>
+                        <th>Categoría</th>
+                        <th>Duración</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody className="text-center">
+                    <tr>
+                        <td>001</td>
+                        <td>Bohemian Rhapsody</td>
+                        <td>Queen</td>
+                        <td>Rock</td>
+                        <td>5:55</td>
+                        <td className="text-center">
+                            <Button variant='success' className='me-2'>
+                                <i className="bi bi-pencil-square"></i>
+                            </Button>
+                            <Button variant="danger">
+                            <i className="bi bi-trash"></i>
+                            </Button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>002</td>
+                        <td>Shape of You</td>
+                        <td>Ed Sheeran</td>
+                        <td>Pop</td>
+                        <td>4:23</td>
+                        <td className="text-center">
+                            <Button variant='success' className='me-2'>
+                                <i className="bi bi-pencil-square"></i>
+                            </Button>
+                            <Button variant="danger">
+                            <i className="bi bi-trash"></i>
+                            </Button>
+                        </td>
+                        
+                    </tr>
+                    <tr>
+                        <td>003</td>
+                        <td>Blinding Lights</td>
+                        <td>The Weeknd</td>
+                        <td>Synth-pop</td>
+                        <td>3:20</td>
+                        <td className="text-center">
+                            <Button variant='success' className='me-2'>
+                                <i className="bi bi-pencil-square"></i>
+                            </Button>
+                            <Button variant="danger">
+                            <i className="bi bi-trash"></i>
+                            </Button>
+                        </td>
+                    </tr>
+                </tbody>
+            </Table>
+        </section>
     );
 };
 
