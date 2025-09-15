dd71807a4f33b3d0e652c03a05fb104d15bcec7e
Tali <talicabana@gmail.com>
2025-09-15 19:11:19 -0300

    Agregué descripción de Acerca de Nosotros

---
+++ src/components/pages/Nosotros.jsx
@@ -0,0 +1,14 @@
+const Nosotros = () => {
+    return (
+        <section>
+         <h2>¿Quienes somos?</h2>
+            <p>En <a href="#" class="text-decoration-none fw-bold">AlgoRitmo</a> creemos que la música es más que un sonido: 
+    es una experiencia que conecta personas, emociones y momentos. Nuestra plataforma de streaming fue creada 
+    para que disfrutes de tus canciones favoritas con la mejor calidad, descubras nuevos artistas y armes 
+    listas que acompañen tu día a día.  
+  </p>
+        </>
+    );
+};
+
+export default Nosotros;
