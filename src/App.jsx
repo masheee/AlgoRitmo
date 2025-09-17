c1fdef6d17f6ec00357ac8e797339a8326741354
Tali <talicabana@gmail.com>
2025-09-17 17:40:57 -0300

    Le di formato al texto

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import { Card, Container } from "react-bootstrap";
 import Menu from "./components/shared/Menu";
 import Footer from "./components/shared/Footer";
-<<<<<<< HEAD
-import Administrador from "./components/pages/Administrador";
-=======
 import Detalle from "./components/pages/Detalle";
 import CardCanciones from "./components/pages/CardCanciones";
 import Administrador from "./components/pages/Administrador";
 import Nosotros from "./components/pages/Nosotros";
->>>>>>> 2447631b3a11c2273f30b5da54b59a6a5e98ee35
 
 export default function AppLayout() {
   return (
